b0ce2485f1c99871dd03eb9995ce6a640742dcf8
moshengwei <moshengwei@krspace.cn>
2017-11-19 22:18:57 +0800

    去掉公共配置类。

--- src/main/java/com/moven/common/prop/JdbcConfig.java
+++ src/main/java/com/moven/common/prop/JdbcConfig.java
@@ -2,8 +2,6 @@ package com.moven.common.prop;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
 /**
  * <p>
